e066b5f71a9ed1f2ed0031771d83e794920e0e0f
Omkar Patil <git21cs072-t@students.git.edu>
2024-04-06 19:44:54 +0530

    Nav implemented!!

---
+++ app/users/new/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NewUserPage = () => {
+  return (
+    <div>
+        <p className='text-black'>Its new users page</p>
+      
+    </div>
+  )
+}
+
+export default NewUserPage
---
+++ app/users/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UserPage = () => {
+  return (
+    <div>
+        <p className='text-black'>Hii its users section</p>
+      
+    </div>
+  )
+}
+
+export default UserPage
