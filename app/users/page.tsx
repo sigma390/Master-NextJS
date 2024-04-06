390b69b76840728148473e5bb255e5232453aff2
Omkar Patil <git21cs072-t@students.git.edu>
2024-04-06 22:25:33 +0530

    Understood static and dynamic rendering!!!

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -11,8 +11,10 @@ interface UsersType {
 const UserPage = async () => {
 
     // http request to bakend
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    //store data
+    // const res = await fetch('https://jsonplaceholder.typicode.com/users');//without cache
+     const res = await fetch('https://jsonplaceholder.typicode.com/users',{
+        cache:'no-store'
+    });//without cache
     const users:UsersType[] = await res.json();
 
 
@@ -20,11 +22,12 @@ const UserPage = async () => {
 
 
   return (
-    <div>
+    <div className='text-black'>
         <p className='text-black text-4xl'>Users</p>
+        <p>{new Date().toLocaleTimeString()}</p> 
         <div className='text-black'>
             {
-                users.map(user =><li key={user.id}>{user.name}</li>
+                users.map(user =><li key={user.id}>{user.name}</li> 
 
                 )
             }
