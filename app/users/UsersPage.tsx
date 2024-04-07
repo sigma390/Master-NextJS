249ab9679e6d8b0732e3b39b6fc1b795c70e068b
Omkar Patil <git21cs072-t@students.git.edu>
2024-04-07 10:12:28 +0530

    BASIC Routing!!

---
+++ app/users/UsersPage.tsx
@@ -0,0 +1,54 @@
+'use client'
+import React from 'react'
+
+
+
+interface UsersType {
+    id:number;
+    name:string;
+    email:string
+}
+
+
+
+
+const UsersPage = async () => {
+    // http request to bakend
+    // const res = await fetch('https://jsonplaceholder.typicode.com/users');//without cache
+    const res = await fetch('https://jsonplaceholder.typicode.com/users',{
+        cache:'no-store'
+    });//without cache
+    const users:UsersType[] = await res.json();
+  return (
+    <div className='text-black'>
+        <p className='text-black text-4xl'>Users</p>
+        <p>{new Date().toLocaleTimeString()}</p> 
+        <table className=' text-black table table-bordered'>
+            <thead >
+                <tr>
+                    <th className='text-black'>Name</th>
+                    <th className='text-black'>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+            {
+                users.map(user =><tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                </tr> 
+
+                )
+            }
+
+            </tbody>
+            
+
+
+
+        </table>
+      
+    </div>
+  )
+}
+
+export default UsersPage
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,56 +1,24 @@
 import React from 'react'
-
-interface UsersType {
-    id:number;
-    name:string;
-    email:string
-}
+import UsersPage from './UsersPage'
 
 
 
 
 const UserPage = async () => {
 
-    // http request to bakend
-    // const res = await fetch('https://jsonplaceholder.typicode.com/users');//without cache
-     const res = await fetch('https://jsonplaceholder.typicode.com/users',{
-        cache:'no-store'
-    });//without cache
-    const users:UsersType[] = await res.json();
+    
 
 
 
 
 
   return (
-    <div className='text-black'>
-        <p className='text-black text-4xl'>Users</p>
-        <p>{new Date().toLocaleTimeString()}</p> 
-        <table className=' text-black table table-bordered'>
-            <thead >
-                <tr>
-                    <th className='text-black'>Name</th>
-                    <th className='text-black'>Email</th>
-                </tr>
-            </thead>
-            <tbody>
-            {
-                users.map(user =><tr key={user.id}>
-                <td>{user.name}</td>
-                <td>{user.email}</td>
-                </tr> 
-
-                )
-            }
-
-            </tbody>
-            
-
-
-
-        </table>
-      
-    </div>
+    <>
+        <h1>Users</h1>
+        <UsersPage/>
+    
+    </>
+    
   )
 }
 
