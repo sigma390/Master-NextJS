4ebd613d38036e11136feea7cc61e1801c15d934
Omkar Patil <git21cs072-t@students.git.edu>
2024-04-12 23:23:32 +0530

    Done delete method

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -34,3 +34,14 @@ export async function PUT(req:NextRequest, {params:{id}}:UserProps){
     }
     return NextResponse.json({id:1, name:body.name});
 }
+// delete 
+
+export function DELETE(req:NextRequest, {params:{id}}:UserProps){
+    //if user exists or not
+
+    if (id>10) {
+        return NextResponse.json({error:'User not found!!!'},{status:404});
+        
+    }
+    return NextResponse.json({});
+}
