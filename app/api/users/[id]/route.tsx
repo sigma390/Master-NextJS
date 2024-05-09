7baaac29f7b75a9af350693192826cea35940fbe
Omkar Patil <git21cs072-t@students.git.edu>
2024-05-09 21:29:23 +0530

    Get single user learnd

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -2,25 +2,35 @@ import { error } from 'console';
 import { NextRequest, NextResponse } from 'next/server';
 import { Schema } from 'zod';
 import { schema } from '../schema';
+import prisma from '../../../../prisma/client';
 
 
 type UserProps = {
     params:{
-        id:number,
+        id:string,
        
     }
 }
 
 
-export function GET(req:NextRequest, {params:{id}}:UserProps){
+export async function GET(req:NextRequest, {params:{id}}:UserProps){
 
-    if (id > 10) {
+    // if (id > 10) {
+    //   return   NextResponse.json({error:'User Not found 404'}, {status:404})
+    // }
+    // return NextResponse.json({
+    //     id:1,
+    //     name:'Omkar'
+    // })
+   const user = await prisma.user.findUnique({
+        where:{id: parseInt(id)} //as the params inn url are string so we convert string to number asin our model the id is number type
+    })
+
+
+    if (!user) {
       return   NextResponse.json({error:'User Not found 404'}, {status:404})
     }
-    return NextResponse.json({
-        id:1,
-        name:'Omkar'
-    })
+    return NextResponse.json(user)
 }
 
 //put request 
@@ -35,7 +45,7 @@ export async function PUT(req:NextRequest, {params:{id}}:UserProps){
         return NextResponse.json(validation.error.errors,{status:400})
         
     }
-    if(id >10){
+    if(id >'10'){
         return NextResponse.json({error:"User not Found!!!"},{status:404})
     }
     return NextResponse.json({id:1, name:body.name});
@@ -45,7 +55,7 @@ export async function PUT(req:NextRequest, {params:{id}}:UserProps){
 export function DELETE(req:NextRequest, {params:{id}}:UserProps){
     //if user exists or not
 
-    if (id>10) {
+    if (id>'10') {
         return NextResponse.json({error:'User not found!!!'},{status:404});
         
     }
